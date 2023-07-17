bc62c062419e725612458a954f78cf1a1c96ab5d
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-07-17 17:37:11 -0300

    Requisito 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+
+
+def exists_word(word: str, instance: Queue):
+    lower_word = word.lower()
+
+    result = []
+    for el in instance.data:
+        ocorrencias = []
+        lines = el['linhas_do_arquivo']
+        arq = el['nome_do_arquivo']
+        for index, w in enumerate(lines):
+            check_word = w.lower()
+            if lower_word in check_word:
+                ocorrencias.append({"linha": index + 1})
+        if len(ocorrencias) > 0:
+            result.append({
+                "palavra": word,
+                "arquivo": arq,
+                "ocorrencias": ocorrencias
+            })
+    return result
 
 
 def search_by_word(word, instance):
